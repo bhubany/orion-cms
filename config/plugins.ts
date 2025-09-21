8d7550dd2aefb8b510e8fc9a1a26aee0c3599f09
bhubany <yadav.bhuban.by@gmail.com>
2025-09-22 00:08:07 +0545

    feat(): add AWS s3 upload as provider

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,30 @@
-export default () => ({});
+export default ({ env }) => {
+  return {
+    upload: {
+      config: {
+        provider: "aws-s3",
+        providerOptions: {
+          baseUrl: env("CDN_URL", undefined),
+          rootPath: env("CDN_ROOT_PATH", undefined),
+          credentials: {
+            accessKeyId: env("AWS_ACCESS_KEY_ID"),
+            secretAccessKey: env("AWS_SECRET_ACCESS_KEY"),
+            sessionToken: env("AWS_SESSION_TOKEN", undefined),
+          },
+          region: env("AWS_REGION", "ap-south-1"),
+          params: {
+            ACL: "private",
+            signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 15 * 60),
+            Bucket: env("AWS_BUCKET"),
+          },
+        },
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          delete: {},
+        },
+      },
+    },
+    // ...
+  };
+};
--- src/api/blog/routes/blog.ts
+++ src/api/blog/routes/blog.ts
@@ -3,13 +3,4 @@
  */
 import { factories } from "@strapi/strapi";
 
-export default factories.createCoreRouter("api::blog.blog", {
-  config: {
-    find: {
-      policies: ["global::project-scope"], // attach policy
-    },
-    findOne: {
-      policies: ["global::project-scope"],
-    },
-  },
-});
+export default factories.createCoreRouter("api::blog.blog");
